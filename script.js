c473974c2d2ac9efefb6d9460a8a24accae51c54
Victor Chan <victorchandev@gmail.com>
2019-10-06 12:15:01 -0400

    working api

---
+++ script.js
@@ -0,0 +1,230 @@
+// Code goes here
+
+/*
+Consumer ID365
+Application TypeWeb
+Application NameMoney Made Ready
+User redirect URL
+Developer Emailwgheath@gmail.com
+App DescriptionHelping banking customers reach specific financial goals by utilizing financial planning, metrics, and goal tracking.
+Consumer Keyggkcnc5pgx0deattmyt04f2z1taopm1z0lqmmbsk
+Consumer Secretjlge4qkapcz4g3fxztzbogcg5wqsjutcwgveom2k
+OAuth Endpointhttps://citizensbank.openbankproject.com/oauth/initiate
+OAuth DocumentationHow to use OAuth for OpenBankProject
+Dummy Users' Direct Login TokensGet dummy users' token
+Direct Login Endpointhttps://citizensbank.openbankproject.com/my/logins/direct
+Direct Login DocumentationHow to use Direct Login
+
+
+Robert.Us.0201Authorization: DirectLogin token="eyJhbGciOiJIUzI1NiJ9.eyIiOiIifQ._WL1_alFDOjxXtmyWr2jE1Sx7nV3Ex-1fnB8HqFQknI"
+Susan.Us.0201Authorization: DirectLogin token="eyJhbGciOiJIUzI1NiJ9.eyIiOiIifQ.dmP0WGOvRGSiccOzgcXCwcqXiEGtFtSC4xHuJZUv2TQ"
+Anil.Us.0201Authorization: DirectLogin token="eyJhbGciOiJIUzI1NiJ9.eyIiOiIifQ.JS7s7vNZHX6HXRrH3FryMNEdS9D2tp5hIeoPWljK3jQ"
+Ellie.Us.0201Authorization: DirectLogin token="eyJhbGciOiJIUzI1NiJ9.eyIiOiIifQ.CtAlOHycwX85iIkuH6WoqYGpNMdX8hUZjH7NSB4OMjQ"
+Rosalie.Us.0201Authorization: DirectLogin token="eyJhbGciOiJIUzI1NiJ9.eyIiOiIifQ.3hE630tP3qyej5Odr5iUWWwZ_927PAaJy1ywqhzGxSw"
+Chris.Us.0201Authorization: DirectLogin token="eyJhbGciOiJIUzI1NiJ9.eyIiOiIifQ.r73R1RuwKQD3vBviucEX0tkya0wZfMiMUa1vKaDHeQk"
+Dennis.Us.0201Authorization: DirectLogin token="eyJhbGciOiJIUzI1NiJ9.eyIiOiIifQ.l6y6vd04HbhfIfnR3RejPG2UgCax89mpPhZeLRwPlyM"
+Jane.Us.0201Authorization: DirectLogin token="eyJhbGciOiJIUzI1NiJ9.eyIiOiIifQ.OFhOeO8fH3g5F3-OiVd88Xjcrm8HaQQSPeyu4n74IrU"
+Emma.Us.0201Authorization: DirectLogin token="eyJhbGciOiJIUzI1NiJ9.eyIiOiIifQ.STPQXM26oTycq7_5OmduW3q9RZQYkStwrO8Y0PzdL4U"
+Sebastien.Us.0201Authorization: DirectLogin token="eyJhbGciOiJIUzI1NiJ9.eyIiOiIifQ.NNdy2wd-g0XHbMoAzjLuzG-2pO0ONXPE0NlP3LSpF08"
+Wim.Us.0201Authorization: DirectLogin token="eyJhbGciOiJIUzI1NiJ9.eyIiOiIifQ.W6n95LkSAFbe5IS2qR3hVSq0mtPir0V5HX8t8Mwn1bE"
+Simon.Us.0201Authorization: DirectLogin token="eyJhbGciOiJIUzI1NiJ9.eyIiOiIifQ.rLXsBMKt47IGICsaZmxET7B-SRUlu3SOzXoEazbuBag"
+Jean-paul.Us.0201Authorization: DirectLogin token="eyJhbGciOiJIUzI1NiJ9.eyIiOiIifQ.G9NnmuJzVPa10rE19yjpop5TGjq74kFigQYui21GStw"
+Elise.Us.0201Authorization: DirectLogin token="eyJhbGciOiJIUzI1NiJ9.eyIiOiIifQ.lOZKsJ1FvSavbCHOs-wiAR3uAZXP0GfV4DffDeyZKLA"
+Eleanor.Us.0201Authorization: DirectLogin token="eyJhbGciOiJIUzI1NiJ9.eyIiOiIifQ.J25NaIYEfmfj2lZGlREM_6vCj0NdYC7RugGEzyJbnJw"
+
+{
+        "user_name":"Robert.Us.0201",
+        "password":"X!e07c40cf",
+        "email":"robert.us.0201@x.y"
+      },
+      {
+        "user_name":"Susan.Us.0201",
+        "password":"X!744b2d0a",
+        "email":"susan.us.0201@x.y"
+      },
+      {
+        "user_name":"Anil.Us.0201",
+        "password":"X!dc2ebaf6",
+        "email":"anil.us.0201@x.y"
+      },
+      {
+        "user_name":"Ellie.Us.0201",
+        "password":"X!e61237b8",
+        "email":"ellie.us.0201@x.y"
+      },
+      {
+        "user_name":"Rosalie.Us.0201",
+        "password":"X!6f94ba71",
+        "email":"rosalie.us.0201@x.y"
+      },
+      {
+        "user_name":"Chris.Us.0201",
+        "password":"X!9f49815c",
+        "email":"chris.us.0201@x.y"
+      },
+      {
+        "user_name":"Dennis.Us.0201",
+        "password":"X!0169aac1",
+        "email":"dennis.us.0201@x.y"
+      },
+      {
+        "user_name":"Jane.Us.0201",
+        "password":"X!f3df0b40",
+        "email":"jane.us.0201@x.y"
+      },
+      {
+        "user_name":"Emma.Us.0201",
+        "password":"X!893554ed",
+        "email":"emma.us.0201@x.y"
+      },
+      {
+        "user_name":"Sebastien.Us.0201",
+        "password":"X!8448ade4",
+        "email":"sebastien.us.0201@x.y"
+      },
+      {
+        "user_name":"Wim.Us.0201",
+        "password":"X!9641f724",
+        "email":"wim.us.0201@x.y"
+      },
+      {
+        "user_name":"Simon.Us.0201",
+        "password":"X!9983cbe5",
+        "email":"simon.us.0201@x.y"
+      },
+      {
+        "user_name":"Jean-paul.Us.0201",
+        "password":"X!e46f14bf",
+        "email":"jean-paul.us.0201@x.y"
+      },
+      {
+        "user_name":"Elise.Us.0201",
+        "password":"X!8735736f",
+        "email":"elise.us.0201@x.y"
+      },
+      {
+        "user_name":"Eleanor.Us.0201",
+        "password":"X!d403bb10",
+        "email":"eleanor.us.0201@x.y"
+      },
+      {
+        "user_name":"Robert.Us.44",
+        "password":"X!252b3849",
+        "email":"robert.us.44@x.y"
+      },{
+        "user_name":"Susan.Us.44",
+        "password":"X!28b651f8",
+        "email":"susan.us.44@x.y"
+      },{
+        "user_name":"Anil.Us.44",
+        "password":"X!aaa38f55",
+        "email":"anil.us.44@x.y"
+      },{
+        "user_name":"Ellie.Us.44",
+        "password":"X!0c896c15",
+        "email":"ellie.us.44@x.y"
+      }
+Please ask a member of the Open Bank Project team for more logins if you require. You can use this application which also uses OAuth to browse your transaction data (use the above username/password).
+
+Power Users
+The following are power user accounts on the sandbox which have permissions to create banks, create products etc.
+
+        {
+            "username": "power.user.0",
+            "password": "X!16a99a40",
+            "email": "power0@tesobe.com"
+        },
+        {
+            "username": "power.user.1",
+            "password": "X!16a99a41",
+            "email": "power1@tesobe.com"
+        },
+        {
+            "username": "power.user.2",
+            "password": "X!16a99a42",
+            "email": "power2@tesobe.com"
+        },
+        {
+            "username": "power.user.3",
+            "password": "X!16a99a43",
+            "email": "power3@tesobe.com"
+        },
+        {
+            "username": "power.user.4",
+            "password": "X!16a99a44",
+            "email": "power4@tesobe.com"
+        },
+        {
+            "username": "power.user.5",
+            "password": "X!16a99a45",
+            "email": "power5@tesobe.com"
+        },
+        {
+            "username": "power.user.6",
+            "password": "X!16a99a46",
+            "email": "power6@tesobe.com"
+        },
+        {
+            "username": "power.user.7",
+            "password": "X!16a99a47",
+            "email": "power7@tesobe.com"
+        },
+        {
+            "username": "power.user.8",
+            "password": "X!16a99a48",
+            "email": "power8@tesobe.com"
+        },
+        {
+            "username": "power.user.9",
+            "password": "X!16a99a49",
+            "email": "power9@tesobe.com"
+        }
+
+*/
+
+$(function() {
+  console.log("_dbg in jquery initialize function")
+  $("#getinfo_btn").click(function(){
+        console.log("button");
+   
+  $.ajax({
+    url: "https://citizensbank.openbankproject.com/obp/v4.0.0/my/banks/citizens.0201.us-b.cb/accounts/69b1fd57-c7df-4b22-ab2b-5f7cc6e73f70/account",
+    //data: myData,
+    //url: "https://crossorigin.me/https://citizensbank.openbankproject.com/obp/v4.0.0/my/banks/citizens.0201.us-b.cb/accounts/69b1fd57-c7df-4b22-ab2b-5f7cc6e73f70/account",
+    
+    type: 'GET',
+    async: true,
+    //crossDomain: true,
+    //dataType: 'jsonp',
+    success: function() { console.log("Success"); },
+    error: function() { console.log('Failed!'); },
+    headers: {
+      "authorization": "DirectLogin token=eyJhbGciOiJIUzI1NiJ9.eyIiOiIifQ._WL1_alFDOjxXtmyWr2jE1Sx7nV3Ex-1fnB8HqFQknI",
+    "cache-control": "no-cache",
+    "postman-token": "8b03836d-2dfa-a3bf-77f3-00c01ba67fb8",
+              'Access-Control-Allow-Origin': '*'
+          },
+    }).done(function(response) {
+        renderTable(response);
+        
+        // function(response) {
+        // console.log(response);
+        // $("#results").html(response.bank.amount);
+      });
+
+});
+
+})
+
+function renderTable(obj) {
+    var results = "";
+
+    results += "<table> <tr> <td> Bank ID: </td> <td>" + obj.bank_id + "</td> </tr>";
+    results += "<tr> <td> Name: </td> <td>" + obj.owners[0].display_name.split(".",1) + "</td> </tr>";
+    results += "<tr> <td> Account Number: </td> <td>" + obj.number + "</td> </tr>";
+    results += "<tr> <td> Account Balance: </td> <td>" + obj.balance.amount + "</td> </tr>";
+    results += "<tr> <td> Currency: </td> <td>" + obj.balance.currency + "</td> </tr>";
+    results += "<tr> <td> IBAN: </td> <td>" + obj.account_routings[0].address + "</td> </tr>";
+    results += "</table>"
+    document.getElementById("results").innerHTML = results;
+};
